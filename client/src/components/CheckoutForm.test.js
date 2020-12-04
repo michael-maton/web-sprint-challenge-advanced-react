e18b594e8ede686c9297403755839b00da5d458d
Michael Maton <mjmaton6@gmail.com>
2020-12-04 11:54:56 -0600

    PlantList and custom hook implemented for MVP

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,42 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+  const header = screen.queryByText(/checkout form/i);
+  expect(header).toBeInTheDocument();
+  expect(header).toBeTruthy();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+  //Arrange: setup react component
+  render(<ContactForm />);
+
+  //Act: Submit our form
+  //  1. Query for our form elements
+  //  2. Type into our form elements
+  //  3. Query for our form buttom
+  //  4. Click our form button
+  const fName = screen.getByLabelText(/First Name:/i);
+  const lName = screen.getByLabelText(/Last Name:/i);
+  const address = screen.getByLabelText(/Address:/i);
+  const city = screen.getByLabelText(/City:/i);
+  const state = screen.getByLabelText(/State:/i);
+  const zip = screen.getByLabelText(/Zip:/i);
+
+  userEvent.type(fName, "Michael");
+  userEvent.type(lName, "Maton");
+  userEvent.type(address, "12805 Tipperary Lane");
+  userEvent.type(city, "Plainfield");
+  userEvent.type(state, "IL");
+  userEvent.type(zip, "60585");
+
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+  //   console.log(button);
+
+  //Assert: Test to see if submit worked
+  //  1. Find our submitted inputs
+  const success = await screen.findByText(/You have ordered some plants!/i);
+  expect(success).toBeInTheDocument();
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,17 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+const useForm = (intitialValue) => {
+  const [values, setValues] = useState(intitialValue);
+
+  const handleChanges = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return [values, handleChanges];
+};
+
+export default useForm;
